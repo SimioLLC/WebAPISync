de5a6bb555284c202b176776ac9494b5bd584ef7
gtwirth <42541127+gtwirth@users.noreply.github.com>
2023-03-02 11:20:23 -0500

    Update to include caching of messages and WebAPIRetrieveIntoOutputTable step.

--- WebAPISync/WebAPIElement.cs
+++ WebAPISync/WebAPIElement.cs
@@ -16,6 +16,10 @@ using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Net;
 using System.Web;
+using System.Xml.Linq;
+using System.Data;
+using System.Xml;
+using Newtonsoft.Json;
 
 namespace WebAPISync
 {
@@ -65,7 +69,13 @@ namespace WebAPISync
         {
             // Example of how to add a property definition to the element.
             IPropertyDefinition pd = schema.PropertyDefinitions.AddStringProperty("BaseURL", "http://localhost:54000");
-            pd.Description = "The Base URL of the http(s) request.";
+            pd.Description = "The Base URL of the http(s) posts.";
+            pd.Required = true;
+
+            pd = schema.PropertyDefinitions.AddBooleanProperty("PersistReceivedMessages");
+            pd.DisplayName = "Persist Received Messages";
+            pd.Description = "Persist Received Messages to be retreived using WebAPIRetrieveIntoOutputTable step";
+            pd.DefaultString = "False";
             pd.Required = true;
 
             _ed = schema.EventDefinitions.AddEvent("ElementEvent");
@@ -99,6 +109,8 @@ namespace WebAPISync
         public System.IO.TextWriter Output { get; set; }
         IWebHost _webhost;
         Task _runningServer;
+        List<string> _receivedMessages = new List<string>();
+        bool _persistReceivedMessages = false;
 
         public WebAPIElement(IElementData data)
         {
@@ -114,6 +126,9 @@ namespace WebAPISync
         {
             IPropertyReader baseURLProp = _data.Properties.GetProperty("BaseURL");
             string baseURL = baseURLProp.GetStringValue(_data.ExecutionContext);
+
+            IPropertyReader persistReceivedMessagesProp = _data.Properties.GetProperty("PersistReceivedMessages");
+            _persistReceivedMessages = Convert.ToBoolean(persistReceivedMessagesProp.GetDoubleValue(_data.ExecutionContext));
 
             _webhost = new WebHostBuilder()
             .UseKestrel()
@@ -133,11 +148,14 @@ namespace WebAPISync
                     {
                         StreamReader reader = new StreamReader(request.Body);
                         _value = reader.ReadToEnd();
+                        if (_persistReceivedMessages == true)
+                        {
+                            _receivedMessages.Add(_value);
+                        }
                         _data.ExecutionContext.Calendar.ScheduleCurrentEvent(null, (obj) =>
                         {
                             _data.Events["ElementEvent"].Fire();
                         });
-                        // My logic
                         response.StatusCode = StatusCodes.Status200OK;
                         return Task.CompletedTask;
                     });
@@ -146,6 +164,159 @@ namespace WebAPISync
             .Build();            
 
             _runningServer = _webhost.StartAsync();            
+        }
+
+        public string[,] GetArrayOfMessages(string stylesheet, int numOfColumns, out string[,] stringArray, out int numOfRows)
+        {
+            var mergedDataSet = new DataSet();
+            mergedDataSet.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            List<string> requestResults = new List<string>();
+
+            if (_receivedMessages.Count > 0)
+            {
+                foreach (string message in _receivedMessages)
+                {
+                    requestResults.Add(ParseDataToXML(message, out var parseError));
+                    if (parseError.Length > 0)
+                    {
+                        throw new Exception(parseError);
+                    }
+                }
+
+                _receivedMessages.Clear();
+            }
+
+            if (requestResults.Count > 0)
+            {
+                foreach (var requestResult in requestResults)
+                {
+                    var transformedResult = Simio.Xml.XsltTransform.TransformXmlToDataSet(requestResult, stylesheet, null);
+                    if (transformedResult.XmlTransformError != null)
+                        throw new Exception(transformedResult.XmlTransformError);
+                    if (transformedResult.DataSetLoadError != null)
+                        throw new Exception(transformedResult.DataSetLoadError);
+                    if (transformedResult.DataSet.Tables.Count > 0) numOfRows = transformedResult.DataSet.Tables[0].Rows.Count;
+                    else numOfRows = 0;
+                    if (numOfRows > 0)
+                    {
+                        transformedResult.DataSet.AcceptChanges();
+                        if (mergedDataSet.Tables.Count == 0) mergedDataSet.Merge(transformedResult.DataSet);
+                        else mergedDataSet.Tables[0].Merge(transformedResult.DataSet.Tables[0]);
+                        mergedDataSet.AcceptChanges();
+                    }
+                    var xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(requestResult);
+                }
+            }
+
+            if (mergedDataSet.Tables.Count == 0 || mergedDataSet.Tables[0].Rows.Count == 0)
+            {
+                numOfRows = 0;
+                stringArray = new string[numOfRows, numOfColumns];
+            }
+            else
+            {
+                numOfRows = mergedDataSet.Tables[0].Rows.Count;
+                stringArray = new string[numOfRows, numOfColumns];
+                int rowNumber = -1;
+                foreach (DataRow dataRow in mergedDataSet.Tables[0].Rows)
+                {
+                    rowNumber++;
+                    for (int col = 0; col < mergedDataSet.Tables[0].Columns.Count; col++)
+                    {
+                        stringArray[rowNumber, col] = dataRow.ItemArray[col].ToString();
+                    }
+                }
+            }
+
+            return stringArray;
+        }
+
+        internal static string ParseDataToXML(string responseString, out string responseError)
+        {
+            responseError = String.Empty;
+
+            // no response
+            if (responseString.Length == 0) return responseString;
+
+            bool isXMLResponse = false;
+            bool isProbablyJSONObject = false;
+            XmlDocument xmlDoc;
+            if (responseString.Contains("xml"))
+            {
+                isXMLResponse = true;
+            }
+            else
+            {
+                isProbablyJSONObject = checkIsProbablyJSONObject(responseString);
+            }
+
+            if (isXMLResponse)
+            {
+                return responseString;
+            }
+            else // Default to assume a JSON response
+            {
+                xmlDoc = JSONToXMLDoc(responseString, isProbablyJSONObject);
+            }
+
+            return xmlDoc.InnerXml;
+        }
+
+        internal static bool checkIsProbablyJSONObject(string resultString)
+        {
+            // We are looking for the first non-whitespace character (and are specifically not Trim()ing here
+            //  to eliminate memory allocations on potentially large (we think?) strings
+            foreach (var theChar in resultString)
+            {
+                if (Char.IsWhiteSpace(theChar))
+                    continue;
+
+                if (theChar == '{')
+                {
+                    return true;
+                }
+                else if (theChar == '<')
+                {
+                    return false;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return false;
+        }
+
+        internal static XmlDocument JSONToXMLDoc(string resultString, bool isProbablyJSONObject)
+        {
+            XmlDocument xmlDoc;
+            resultString = resultString.Replace("@", string.Empty);
+            if (isProbablyJSONObject == false)
+            {
+                var prefix = "{ items: ";
+                var postfix = "}";
+
+                using (var combinedReader = new StringReader(prefix)
+                                            .Concat(new StringReader(resultString))
+                                            .Concat(new StringReader(postfix)))
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        Converters = { new Newtonsoft.Json.Converters.XmlNodeConverter() { DeserializeRootElementName = "data" } },
+                        DateParseHandling = DateParseHandling.None,
+                    };
+                    using (var jsonReader = new JsonTextReader(combinedReader) { CloseInput = false, DateParseHandling = DateParseHandling.None })
+                    {
+                        xmlDoc = JsonSerializer.CreateDefault(settings).Deserialize<XmlDocument>(jsonReader);
+                    }
+                }
+            }
+            else
+            {
+                xmlDoc = Newtonsoft.Json.JsonConvert.DeserializeXmlNode(resultString, "data");
+            }
+            return xmlDoc;
         }
 
         /// <summary>
---
+++ WebAPISync/WebAPIRetrieveIntoOutputTable.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Globalization;
+using SimioAPI;
+using SimioAPI.Extensions;
+
+namespace WebAPISync
+{
+    public class WebAPIRetrieveIntoOutputTableDef : IStepDefinition
+    {
+        #region IStepDefinition Members
+
+        /// <summary>
+        /// Property returning the full name for this type of step. The name should contain no spaces. 
+        /// </summary>
+        public string Name
+        {
+            get { return "WebAPIRetrieveIntoOutputTable"; }
+        }
+
+        /// <summary>
+        /// Property returning a short description of what the step does.  
+        /// </summary>
+        public string Description
+        {
+            get { return "The WebAPIRetrieveIntoOutputTable step is used to read persisted message(s)."; }
+        }
+
+        /// <summary>
+        /// Property returning an icon to display for the step in the UI. 
+        /// </summary>
+        public System.Drawing.Image Icon
+        {
+            get { return null; }
+        }
+
+        /// <summary>
+        /// Property returning a unique static GUID for the step.  
+        /// </summary>
+        public Guid UniqueID
+        {
+            get { return MY_ID; }
+        }
+        static readonly Guid MY_ID = new Guid("{b03739ac-d65b-4c04-ae91-ef5ada8d77e2}");
+
+        /// <summary>
+        /// Property returning the number of exits out of the step. Can return either 1 or 2. 
+        /// </summary>
+        public int NumberOfExits
+        {
+            get { return 1; }
+        }
+
+        /// <summary>
+        /// Method called that defines the property schema for the step.
+        /// </summary>
+        public void DefineSchema(IPropertyDefinitions schema)
+        {
+            IPropertyDefinition pd;
+
+            pd = schema.AddElementProperty("WebAPIElement", WebAPIElementDefinition.MY_ID);
+            pd.DisplayName = "WebAPIElement";
+            pd.Description = "WebAPIElement holding messsge receipts";
+            pd.Required = true;    
+
+            pd = schema.AddTableReferenceProperty("DestinationTable");
+            pd.DisplayName = "Destination Table";
+            pd.Description = "Simio destination table";
+            pd.Required = true;
+
+            pd = schema.AddBooleanProperty("ClearRowsBeforeGettingMessages");
+            pd.DisplayName = "Clear Rows Before Getting Messages";
+            pd.Description = "Clear Rows Before Getting Messages.";
+            pd.DefaultString = "True";
+            pd.Required = true;
+
+            pd = schema.AddStringProperty("Stylesheet", @"<xsl:stylesheet version=""1.0"" xmlns:xsl=""http://www.w3.org/1999/XSL/Transform"">
+    <xsl:template match=""node()|@*"">
+      <xsl:copy>
+        <xsl:apply-templates select=""node()|@*""/>
+      </xsl:copy>
+    </xsl:template>
+</xsl:stylesheet>");
+            pd.DisplayName = "Stylesheet";
+            pd.Description = "Stylesheet.";
+            pd.Required = true;
+        }
+
+        /// <summary>
+        /// Method called to create a new instance of this step type to place in a process. 
+        /// Returns an instance of the class implementing the IStep interface.
+        /// </summary>
+        public IStep CreateStep(IPropertyReaders properties)
+        {
+            return new WebAPIRetrieveIntoOutputTable(properties);
+        }
+
+        #endregion
+    }
+
+    class WebAPIRetrieveIntoOutputTable : IStep
+    {
+        IPropertyReaders _props;
+        IElementProperty _webAPIElementProp;
+        ITableReferencePropertyReader _destinationTableReaderProp;
+        IPropertyReader _clearRowsBeforeGettingMessagesProp;
+        IPropertyReader _stylesheetTopicProp;
+
+        public WebAPIRetrieveIntoOutputTable(IPropertyReaders properties)
+        {
+            _props = properties;
+            _webAPIElementProp = (IElementProperty)_props.GetProperty("WebAPIElement");
+            _destinationTableReaderProp = (ITableReferencePropertyReader)_props.GetProperty("DestinationTable");
+            _clearRowsBeforeGettingMessagesProp = (IPropertyReader)_props.GetProperty("ClearRowsBeforeGettingMessages");
+            _stylesheetTopicProp = _props.GetProperty("Stylesheet");
+        }
+
+        #region IStep Members
+
+        /// <summary>
+        /// Method called when a process token executes the step.
+        /// </summary>
+        public ExitType Execute(IStepExecutionContext context)
+        {
+            WebAPIElement webAPIElementProp = (WebAPIElement)_webAPIElementProp.GetElement(context);
+            ITableRuntimeData sourceTable = _destinationTableReaderProp.GetTableReference(context);
+            double clearRowsBeforeGettingMessagesDouble = _clearRowsBeforeGettingMessagesProp.GetDoubleValue(context);
+            bool clearRowsBeforeGettingMessages = false;
+            if (clearRowsBeforeGettingMessagesDouble > 0) clearRowsBeforeGettingMessages = true;
+            if (clearRowsBeforeGettingMessages == true) sourceTable.RemoveAllRows(context);
+            String stylesheet = _stylesheetTopicProp.GetStringValue(context);
+
+            int numOfColumns = sourceTable.Table.Columns.Count + sourceTable.Table.StateColumns.Count;
+
+            string[,] parts = webAPIElementProp.GetArrayOfMessages(stylesheet, numOfColumns, out string[,] stringArray, out int numOfRows);
+
+            int numReadIn = 0;
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                ITableRuntimeDataRow row = sourceTable.AddRow(context);
+                for (int j = 0; j < numOfColumns; j++)
+                {
+                    // Resolve the property value to get the runtime state
+                    IState state = row.States[j];
+                    string part = parts[i, j];
+
+                    if (TryAsNumericState(state, part) ||
+                        TryAsDateTimeState(state, part) ||
+                        TryAsStringState(state, part))
+                    {
+                        numReadIn++;
+                    }
+                }
+            }
+
+            context.ExecutionInformation.TraceInformation(String.Format("Messages(s) have been read from endpoint URL."));
+
+            // We are done reading, have the token proceed out of the primary exit
+            return ExitType.FirstExit;
+        }
+
+        bool TryAsNumericState(IState state, string rawValue)
+        {
+            IRealState realState = state as IRealState;
+            if (realState == null)
+                return false; // destination state is not a real.
+
+            double d = 0.0;
+            if (Double.TryParse(rawValue, NumberStyles.Any, CultureInfo.InvariantCulture, out d))
+            {
+                realState.Value = d;
+                return true;
+            }
+            else if (String.Compare(rawValue, "True", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                realState.Value = 1.0;
+                return true;
+            }
+            else if (String.Compare(rawValue, "False", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                realState.Value = 0.0;
+                return true;
+            }
+
+            return false; // incoming value can't be interpreted as a real.
+        }
+
+        bool TryAsDateTimeState(IState state, string rawValue)
+        {
+            IDateTimeState dateTimeState = state as IDateTimeState;
+            if (dateTimeState == null)
+                return false; // destination state is not a DateTime.
+
+            DateTime dt;
+            if (DateTime.TryParse(rawValue, CultureInfo.InvariantCulture, DateTimeStyles.None, out dt))
+            {
+                dateTimeState.Value = dt;
+                return true;
+            }
+
+            // If it isn't a DateTime, maybe it is just a number, which we can interpret as hours from start of simulation.
+            double d = 0.0;
+            if (Double.TryParse(rawValue, NumberStyles.Any, CultureInfo.InvariantCulture, out d))
+            {
+                state.StateValue = d;
+                return true;
+            }
+
+            return false;
+        }
+
+        bool TryAsStringState(IState state, string rawValue)
+        {
+            IStringState stringState = state as IStringState;
+            if (stringState == null)
+                return false; // destination state is not a string.
+
+            // Since all input value are already strings, this is easy.
+            stringState.Value = rawValue;
+            return true;
+        }
+
+        #endregion
+    }
+}
